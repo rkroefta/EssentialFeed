108da5a11de6c71b26a4fdb81127de7b8f7bab2c
Rodrigo Kroef Tarouco <142683014+rkroefta@users.noreply.github.com>
2024-04-02 10:34:20 +0200

    Move the URLSessionHTTPClient( the system under test, or "SUT) creation to a factory method.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -44,7 +44,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
     }
 
@@ -53,17 +53,15 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "anz error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
 
-        let sut = URLSessionHTTPClient()
-
         let exp = expectation(description: "Wait for completion")
 
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertNotNil(receivedError)
             default:
                 XCTFail("Expected failure with error \(error), got result instead")
-            }
+            } 
 
             exp.fulfill()
         }
@@ -73,6 +71,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
 
     //MARK: - Helpers
+
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
 
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
