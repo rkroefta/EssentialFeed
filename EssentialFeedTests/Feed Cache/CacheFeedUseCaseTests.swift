4467183830c4691682ac2a5e6cb2a7aa32c362f4
Rodrigo Kroef Tarouco <142683014+rkroefta@users.noreply.github.com>
2024-05-20 09:54:34 +0200

    Guarantee that the LocalFeedLoader does not deliver deletion error after instsance has been deallocated

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -18,7 +18,8 @@ class LocalFeedLoader {
     }
 
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [unowned self] error in
+        store.deleteCachedFeed { [ weak self] error in
+            guard let self = self else { return }
             if error == nil {
                 self.store.insert(items, timestamp: self.currentDate(), completion: completion)
             } else {
@@ -102,6 +103,19 @@ class CacheFeedUseCaseTests: XCTestCase {
             store.completeDeletionSucessfully()
             store.completeInsertionSucessfully()
         })
+    }
+
+    func test_save_doesNotDeliversDeletionErrorAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+
+        var receveidResults = [Error?]()
+        sut?.save([uniqueItem()])  { receveidResults.append($0) }
+
+        sut = nil
+        store.completeDeletion(with: NSError())
+
+        XCTAssertTrue(receveidResults.isEmpty)
     }
 
 
